a8dd2e9e59cd585b72c33e3433ad8b2938dbf222
Admin <Admin@DESKTOP-08VVHQ8>
2022-11-09 13:54:14 +0530

    My new github project

---
+++ src/Sample1/Test1.java
@@ -0,0 +1,16 @@
+package Sample1;
+
+public class Test1 {
+	
+	 public static void main(String[]args)                 //main method
+	 {
+		                                                   //main method body
+		 
+		 System.out.println("main method is running");     //printing statement
+		 
+         System.out.println("main method is available");  //printing statement
+		 
+	               
+	}
+
+}
